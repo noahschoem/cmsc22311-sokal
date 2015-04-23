6a77c1a9f6b5d220b66daaf59c894af97bf9729a
Noah Schoem <nschoem@gmail.com>
2015-04-23 11:05:22 -0500

    Installed Text.HTML.TagSoup

--- dist/build/autogen/Paths_sokal.hs
+++ dist/build/autogen/Paths_sokal.hs
@@ -1,7 +1,7 @@
 module Paths_sokal (
     version,
     getBinDir, getLibDir, getDataDir, getLibexecDir,
-    getDataFileName
+    getDataFileName, getSysconfDir
   ) where
 
 import qualified Control.Exception as Exception
@@ -12,21 +12,22 @@ import Prelude
 catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
 catchIO = Exception.catch
 
-
 version :: Version
-version = Version {versionBranch = [0,1,0,0], versionTags = []}
-bindir, libdir, datadir, libexecdir :: FilePath
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/home/nschoem/.cabal/bin"
-libdir     = "/home/nschoem/.cabal/lib/sokal-0.1.0.0/ghc-7.6.3"
-datadir    = "/home/nschoem/.cabal/share/sokal-0.1.0.0"
-libexecdir = "/home/nschoem/.cabal/libexec"
+bindir     = "/home/nschoem/Documents/UChicago/yr4/cs22311/labs/sokal/.cabal-sandbox/bin"
+libdir     = "/home/nschoem/Documents/UChicago/yr4/cs22311/labs/sokal/.cabal-sandbox/lib/x86_64-linux-ghc-7.6.3/sokal-0.1.0.0"
+datadir    = "/home/nschoem/Documents/UChicago/yr4/cs22311/labs/sokal/.cabal-sandbox/share/x86_64-linux-ghc-7.6.3/sokal-0.1.0.0"
+libexecdir = "/home/nschoem/Documents/UChicago/yr4/cs22311/labs/sokal/.cabal-sandbox/libexec"
+sysconfdir = "/home/nschoem/Documents/UChicago/yr4/cs22311/labs/sokal/.cabal-sandbox/etc"
 
-getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "sokal_bindir") (\_ -> return bindir)
 getLibDir = catchIO (getEnv "sokal_libdir") (\_ -> return libdir)
 getDataDir = catchIO (getEnv "sokal_datadir") (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "sokal_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "sokal_sysconfdir") (\_ -> return sysconfdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
--- suck.hs
+++ suck.hs
@@ -1,3 +1,4 @@
 import Distribution.Simple
+import Text.HTML.TagSoup
 
 main = print "Hello World!"
