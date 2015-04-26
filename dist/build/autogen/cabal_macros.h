7a0ef9dd5a614385168f6c55388877543473e0a3
Noah Schoem <nschoem@gmail.com>
2015-04-26 15:35:04 -0500

    Finished writing toPrimitiveModel, now doing filter isAscii before parseTags in removeHTMLCrap, and using 2urls.txt for testing.

--- dist/build/autogen/cabal_macros.h
+++ dist/build/autogen/cabal_macros.h
@@ -21,6 +21,13 @@
   (major1) == 4 && (major2) <  6 || \
   (major1) == 4 && (major2) == 6 && (minor) <= 0)
 
+/* package containers-0.5.0.0 */
+#define VERSION_containers "0.5.0.0"
+#define MIN_VERSION_containers(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  5 || \
+  (major1) == 0 && (major2) == 5 && (minor) <= 0)
+
 /* package tagsoup-0.13.3 */
 #define VERSION_tagsoup "0.13.3"
 #define MIN_VERSION_tagsoup(major1,major2,minor) (\
--- suck.hs
+++ suck.hs
@@ -3,45 +3,50 @@ import Control.Monad
 import Network.HTTP
 import Network.Stream
 import Text.HTML.TagSoup
-import SuckUtils
+import qualified Data.Map as M
+import Data.Char
 
 {-main = simpleHTTP (getRequest url) >>= getResponseBody >>= putStr
 where url = "http://wiki.haskell.org/Typeclassopedia"-}
 
+-- TODO: replace 2urls.txt for testing purposes to urls.txt
 main = do
-  urls <- readFile "1url.txt" >>= (return.lines)
+  urls <- readFile "2urls.txt" >>= (return.lines)
   wepage_responses <- forM urls (\url -> simpleHTTP (getRequest url))
   raw_data <- mapM getResponseBody wepage_responses -- I should probably change this to catch and warn of connection errors
-  print $ head $ map removeHTMLCrap raw_data
+  let raw_text = map removeHTMLCrap raw_data in
+    print $ process raw_text
+    
+type PrimitiveModel = M.Map (String,String) [String]
 
-  
-removeHTMLCrap = extractText . extractDivBody . (dropWhile (/= TagOpen "div" [("id","body")])). canonicalizeTags . parseTags where
+type ProcessedModel = [(String,[(Int,Int)])]
+    
+-- TODO: Make sure that adding filter isAscii at the beginning doesn't break words from webpages too horribly, like turning Gödel into Gdel.
+removeHTMLCrap :: String -> String
+removeHTMLCrap = extractText . extractDivBody . (dropWhile (/= TagOpen "div" [("id","body")])). canonicalizeTags . parseTags . (filter isAscii) where
 --   removeQuotesAndCommas = filter (\x -> notElem x ['\,','"]
   extractText [] = []
   extractText (tag:tags)
-    | tag ~== TagText "" = fromTagText tag ++ extractText tags  -- We are padding whitespace here just in case.
+    | tag ~== TagText "" = fromTagText tag ++ extractText tags
     | otherwise = extractText tags
   extractDivBody = iterDiv 1 . tail where
     iterDiv 0 _  = []
     iterDiv n ((TagOpen "div" attribs):tags)  = (TagOpen "div" attribs):(iterDiv (n+1) tags)
     iterDiv n ((TagClose "div"):tags) = (TagClose "div"):(iterDiv (n-1) tags)
     iterDiv n (tag:tags) = tag:(iterDiv n tags)
-  
--- type PrimitiveModel = Map (String,String) [String]
+    
+process = {-toProcessedModel .-} toPrimitiveModel
 
--- type ProcessedModel = [(String,[(Int,Int)])]
-  
-{-  
--- responsible only for stripping tags from a Response.
-stripTags :: Response -> String
-process  = extractBody . removeAttributeDivs . canonicalizeTags . parseTags . rspBody where
-  removeAttributeDivs tagSoup = 
-  extractBody = (takeWhile != TagClose "div" []) . tail . (dropWhile (!= TagOpen "div" ("id","body")))
-  
--- takes the webpage text stripped of its tags and spits out a primitive model.
+-- takes webpage text stripped of its HTML tags and spits out a primitive model.
 toPrimitiveModel :: [String] -> PrimitiveModel
-toPrimitiveModel = toPrimitiveModel
-
--- Takes a primitive model and spits out a processed model.
-toProcessedModel :: PrimitiveModel -> ProcessedModel
-toProcessedModel = toProcessedModel-}
+toPrimitiveModel s = prim_model where
+  stage1 = map words s
+  stage2 = map (\x -> (x `zip` tail x) `zip` (map (\a -> [a]) (tail (tail x)))) stage1
+  stage3 = concat stage2
+  prim_model = M.fromListWith (++) stage3
+  
+-- Takes a primitive model and spits out a processed model.  
+toProcessedModel prim = proc where
+  proc = prim
+  
+  
