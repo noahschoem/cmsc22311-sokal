15690a8d05e8839954c90fdd81ed8555d4468c52
Noah Schoem <nschoem@gmail.com>
2015-04-28 23:53:43 -0500

    Implemented commandline arguments.

--- spew.hs
+++ spew.hs
@@ -5,23 +5,65 @@ import System.IO
 import Data.Array
 import System.Random
 import System.Environment
-import System.Exit
+import Data.List
+import System.Console.GetOpt
+import Data.Maybe
 
-main = do
-  {-putStrLn "Seed? Press enter for default."
-  seedIn <- getLine
-  case seedIn of
-       [] -> return ()
-       _  -> setStdGen $ read seedIn
-  putStrLn "Number of words? Default is 100."
-  wordCount <- getLine-}
+-- The following is simply for handling command line options.  I probably went a little overboard.
+-- This is taken from 
+data Options = Options {optLength :: Int, optSeed :: Maybe Int, optHelp :: Bool} deriving (Eq,Show)
+               
+defaultOptions :: Options
+defaultOptions = Options { optLength = 100, optSeed = Nothing, optHelp = False}
+
+options :: [OptDescr (Options -> Options)]
+options = 
+  [ Option ['l'] ["length"] 
+      (ReqArg 
+        (\i opts -> opts { optLength = read i }) 
+        "L")
+      "desired length of spewed text"
+  , Option ['s'] ["seed"]
+      (ReqArg 
+        (\s opts -> opts {optSeed = Just (read s)})
+        "S")
+      "user-supplied seed, if the user so desires"
+  , Option ['h'] ["help"]
+      (NoArg
+        (\opts -> opts {optHelp = True}))
+      "show this help text"
+  ]
+
+dumpHelp :: IO ()
+dumpHelp = do
+  prg <- getProgName
+  hPutStrLn stderr (usageInfo prg options)
+  
+main = do   
+  (actions,_,_) <- getArgs >>= return . (getOpt Permute options)
+  -- this may terminate the program if the optHelp flag has been set
+  let opts = foldr id defaultOptions actions
+  if (optHelp opts) then dumpHelp
+                    else genSpew (optLength opts) (optSeed opts)
+
+genSpew :: Int -> Maybe Int -> IO ()
+genSpew len seed = do
   inputModel <- readFile "sokal.model" >>= (return.lines)
-  setStdGen (mkStdGen 0) --debugging to ensure a consistent random number generator
+  case seed of
+       Just a -> setStdGen (mkStdGen a)
   gen <- getStdGen
   let fastModel = listArray (0,length inputModel - 1) (map read inputModel) :: FastModel
   let ws = evalState (runModel fastModel) gen
-  print $ take 100 ws
+  print $ intercalate " " $ takeEnough len ws
   
+takeEnough :: Int -> [String] -> [String]
+takeEnough n (x:xs)
+  | n > 0     = x:(takeEnough (n-1) xs)
+  | otherwise = takeUntilEndOfSentence (x:xs) where
+    takeUntilEndOfSentence (x:xs)
+      | last x == '.' = [x]
+      | otherwise     = x:(takeUntilEndOfSentence xs)
+
 -- FastModel at index i is an ordered pair containing the encoding string of i and a list of ordered pairs of ints
 -- of the form (weight,another index or -1)
 type FastModel = Array Int (String,[(Int,Int)])
@@ -31,7 +73,6 @@ type RandState = State StdGen
 -- This is heavily inspired (read: copied verbatim then modified) from Lecture 18 from 16100.
 markovSelect :: [(Int,Int)] -> RandState Int
 markovSelect successors = fmap (weightedSelect successors) . state . randomR $ (0,sum $ (map fst) successors) where
-  weightedSelect [] ix = error "Should not get an []"
   weightedSelect ((weight,state):remainders) ix
     | ix - weight <= 0   = state
     | otherwise = weightedSelect remainders (ix-weight)
