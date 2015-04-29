8af5078cb84e2574752af0b46f61c739e0a343ce
Noah Schoem <nschoem@gmail.com>
2015-04-29 12:08:59 -0500

    A little bit of cleanup.

--- spew.hs
+++ spew.hs
@@ -6,26 +6,27 @@ import System.IO
 import Control.Monad.State.Lazy
 import SpewUtils
 import Data.Char
+import System.Exit
 
 -- |The following is simply for handling command line options.  I probably went a little overboard on getting these to work.
-data Options = Options {optLength :: Int, optSeed :: Maybe Int, optShowSeed :: Bool, optHelp :: Bool} deriving (Eq,Show)
+data Options = Options {optLength :: String, optSeed :: Maybe String, optShowSeed :: Bool, optHelp :: Bool} deriving (Eq,Show)
                
 -- |defaultOptions: the default program options.
 defaultOptions :: Options
-defaultOptions = Options { optLength = 100, optSeed = Nothing, optShowSeed = False, optHelp = False}
+defaultOptions = Options { optLength = "100", optSeed = Nothing, optShowSeed = False, optHelp = False}
 
 -- |options mapping.
 options :: [OptDescr (Options -> Options)]
 options = 
   [ Option ['l'] ["length"] 
       (ReqArg 
-        (\i opts -> opts { optLength = read i }) 
-        "L")
+        (\i opts -> opts { optLength = i }) 
+        "m")
       "desired minumum length of spewed text.  Default is 100."
   , Option ['s'] ["seed"]
       (ReqArg 
-        (\s opts -> opts {optSeed = Just (read s)})
-        "S")
+        (\s opts -> opts {optSeed = Just s})
+        "n")
       "for the user to supply their own seed for the random number generator, if so desired"
   , Option ['o'] ["show-seed"]
       (NoArg
@@ -47,21 +48,28 @@ main = do
   -- this may terminate the program if the optHelp flag has been set
   let opts = foldr id defaultOptions actions
   if (optHelp opts) then dumpHelp
-                    else genSpew opts
+                    else sanityCheckLength opts >> genSpew opts
 
+sanityCheckLength :: Options -> IO ()
+sanityCheckLength opts = if and $ (map isDigit) (optLength opts)
+                            then return ()
+                            else hPutStrLn stderr "Error: length should be an integer." >> (exitWith $ ExitFailure 1)
+    
+                    
 genSpew :: Options -> IO ()
 genSpew opts = do
   inputModel <- readFile "sokal.model" >>= (return.lines)
   case optSeed opts of
-       Just a  -> setStdGen (mkStdGen a)
+       Just a  -> setStdGen (read a)
        Nothing -> return ()
   gen <- getStdGen
   if optShowSeed opts
      then hPutStrLn stderr $ "Seed is " ++ show gen
      else return ()
   let ws = evalState (SpewUtils.runModel inputModel) gen
-  -- the entire purpose of zipWith ($) (toUpper:(repeat id)) is because I got really tired of seeing the first letter of output in lower case.
-  putStr $ zipWith ($) (toUpper:(repeat id))  (linefill 72  (takeEnough (optLength opts) ws))
+  -- the entire purpose of capitalizeBeginning is because I got really tired of seeing the first letter of output in lower case.
+  putStr $ capitalizeBeginning (linefill 72  (takeEnough (read (optLength opts)) ws)) where
+     capitalizeBeginning = zipWith ($) (toUpper:(repeat id))
 
 -- |takeEnough: generates n words, then keeps generating words until the end of a sentence is reached.
 takeEnough :: Int -> [String] -> [String]
